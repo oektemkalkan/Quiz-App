090cc0457b562905cfa9ad46a80e346cec7b8a7e
Öktem Kalkan <oektem-kalkan@t-online.de>
2023-04-10 16:21:32 +0200

    finished toggle function

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
-const bodydark = document.querySelector('[data-js="body"]')
-const togglebutton = document.querySelector('[data-js="toggle_js"]');
-toggleColorbutton.addEventListener("click", () => {
+const bodydark = document.querySelector('[data-js="body"]');
+const toggleColorButton = document.querySelector('[data-js="toggle_js"]');
+toggleColorButton.addEventListener("click", () => {
     bodydark.classList.toggle("dark");
 });
