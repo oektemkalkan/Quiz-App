eba26b2594fbc9f0fe90a2f0b829cdfdc4a54f34
Öktem Kalkan <oektem-kalkan@t-online.de>
2023-04-27 11:46:46 +0200

    add form

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,5 +1,34 @@
 const bodydark = document.querySelector('[data-js="body"]');
 const toggleColorButton = document.querySelector('[data-js="toggle_js"]');
-toggleColorButton.addEventListener("click", () => {
-    bodydark.classList.toggle("dark");
+toggleColorButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  bodydark.classList.toggle("dark");
 });
+
+/*
+const index = document.querySelector('[data-js="index"]');
+form.addEventListener("click", (e) => {
+    e.preventDefault();
+    location.replace('index.html');
+});
+
+const bookmark = document.querySelector('[data-js="bookmark"]');
+form.addEventListener("click", (e) => {
+    e.preventDefault();
+    location.href = 'bookmark.html';
+});
+
+const form = document.querySelector('[data-js="form"]');
+form.addEventListener("click", (e) => {
+    e.preventDefault();
+    location.href = 'form.html';
+});
+
+const indexPage = 'http://127.0.0.1:3000/index.html';
+window.history.replace(indexPage);
+
+function Test()
+  {
+    document.getElementById("test").href="index.html"; 
+  }
+*/
